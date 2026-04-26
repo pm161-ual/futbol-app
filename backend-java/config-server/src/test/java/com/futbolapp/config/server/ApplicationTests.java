41d67ee68d98ae1029d5c6d808b909e655d7117b
Patricio <pm161@inlumine.ual.es>
2026-04-26 11:38:47 +0200

    feat: eureka-server funcionando en puerto 8761

--- backend-java/config-server/src/test/java/com/futbolapp/config/server/ApplicationTests.java
+++ backend-java/config-server/src/test/java/com/futbolapp/config/server/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.futbolapp.eureka_server;
+package com.futbolapp.config.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
