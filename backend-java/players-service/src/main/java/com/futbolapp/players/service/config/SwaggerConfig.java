31600917aaf7dd61a278e266ca255b182d8b228a
Patricio <pm161@inlumine.ual.es>
2026-04-28 01:15:07 +0200

    feat: Swagger/OpenAPI en players-service y comments-service con config-server

---
+++ backend-java/players-service/src/main/java/com/futbolapp/players/service/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.futbolapp.players.service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${api.title}")
+    private String title;
+
+    @Value("${api.description}")
+    private String description;
+
+    @Value("${api.contact.name}")
+    private String contactName;
+
+    @Value("${api.contact.email}")
+    private String contactEmail;
+
+    @Value("${version}")
+    private String version;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(version)
+                        .contact(new Contact()
+                                .name(contactName)
+                                .email(contactEmail)));
+    }
+}
